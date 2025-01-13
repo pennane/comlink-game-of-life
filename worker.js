9057bc67184ff4ecc6f0cef601abfaf8871568f3
Arttu Pennanen <arttu@pennanen.org>
2025-01-14 00:53:50 +0200

    consistent naming of now / next

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");
 
-let data_size, image_a, image_b, neighbor_offsets;
+let data_size, image_now, image_next, neighbor_offsets;
 
 function init_worker(w, h) {
   data_size = w * h * 4
@@ -18,27 +18,27 @@ function init_worker(w, h) {
     row_size + 4,   // down-right
   ];
 
-  const buffer_a = new Uint8ClampedArray(data_size);
-  const buffer_b = new Uint8ClampedArray(data_size);
+  const buffer_now = new Uint8ClampedArray(data_size);
+  const buffer_next = new Uint8ClampedArray(data_size);
 
   const opacity = 255
   for (let i = 0; i < data_size; i += 4) {
     const state = Math.random() > 0.9 ? 255 : 0
-    buffer_a[i + 3] = opacity;
-    buffer_b[i + 3] = opacity;
-    buffer_a[i] = state;
-    buffer_b[i] = state;
+    buffer_now[i + 3] = opacity;
+    buffer_next[i + 3] = opacity;
+    buffer_now[i] = state;
+    buffer_next[i] = state;
   }
 
-  image_a = new ImageData(buffer_a, w, h);
-  image_b = new ImageData(buffer_b, w, h);
+  image_now = new ImageData(buffer_now, w, h);
+  image_next = new ImageData(buffer_next, w, h);
 }
 
 function next_frame() {
-  let temp = image_a; image_a = image_b; image_b = temp;
+  let temp = image_now; image_now = image_next; image_next = temp;
 
-  const buffer_now = image_a.data;
-  const buffer_next = image_b.data;
+  const buffer_now = image_now.data;
+  const buffer_next = image_next.data;
 
   for (let i = 4; i < data_size - 4; i += 4) {
     let neighbors = 0;
@@ -58,7 +58,7 @@ function next_frame() {
     buffer_next[i] = state_next;
     buffer_next[i + 3] = 255;
   }
-  return image_b
+  return image_next
 }
 
 Comlink.expose({ next_frame, init_worker });
